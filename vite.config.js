a25cdc834f1406f01b04e0d1774233bab5ad7afa
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2025-05-23 10:56:34 +0300

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // Allow external access for dev server
+    port: 5173         // Optional: You can specify a port
+  },
+  preview: {
+    host: '0.0.0.0',   // Ensure preview server also binds to 0.0.0.0
+    port: 5173         // Optional: Ensure consistent port
+  }
 })
