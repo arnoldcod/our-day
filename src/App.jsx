81ea57c8ff0c829ab69593bb7393ea86f7127188
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2025-10-07 06:46:04 +0300

    modified the navbar and commented out the itinerary navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from './components/HomePage';
 import OurStoryPage from './components/OurStoryPage';
 import RegistryPage from './components/RegistryPage';
 import RsvpPage from './components/RsvpPage';
-import ItineraryPage from './components/ItineraryPage';
+// import ItineraryPage from './components/ItineraryPage';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/our-story" element={<OurStoryPage />} />
         <Route path="/registry" element={<RegistryPage />} />
         <Route path="/rsvp" element={<RsvpPage />} />
-        <Route path="/itinerary" element={<ItineraryPage />} />
+        {/* <Route path="/itinerary" element={<ItineraryPage />} /> */}
       </Routes>
     </Router>
   );
